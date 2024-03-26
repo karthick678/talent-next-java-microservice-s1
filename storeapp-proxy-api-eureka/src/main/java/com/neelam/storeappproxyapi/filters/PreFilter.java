09dd600b89042a2fcf1130889f7e6ee5b5350b4d
karthick678 <karthickkumar678@gmail.com>
2024-03-26 16:21:25 +0530

    - Implemented the API Gateway (zuul)

---
+++ storeapp-proxy-api-eureka/src/main/java/com/neelam/storeappproxyapi/filters/PreFilter.java
@@ -0,0 +1,35 @@
+package com.neelam.storeappproxyapi.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PreFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        System.out.println("----------------------------------");
+        System.out.println("\n Request Method: " + request.getMethod()
+                + "Request Url: " + request.getRequestURL().toString());
+        return null;
+    }
+}
